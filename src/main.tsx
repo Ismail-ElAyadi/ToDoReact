125c64ebb5100fae30f31a783ebf12d4c604129a
Ismail <ismail.elydi@gmail.com>
2020-01-21 01:02:29 +0100

    environnement ready

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import { render } from "react-dom";
+import * as React from "react";
+// import { h, render } from "preact";
+render(
+  <div> salut les bonjour </div>,
+  document.getElementById("app") as Element
+);
